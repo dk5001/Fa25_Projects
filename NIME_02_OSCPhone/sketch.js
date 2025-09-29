1386733d8d2244aeff6defce0c8b2da8e182773e
fl4meboi <djkim9876@gmail.com>
2025-09-29 14:31:34 -0400

    added controllable params in ableton

--- NIME_02_OSCPhone/sketch.js
+++ NIME_02_OSCPhone/sketch.js
@@ -27,14 +27,14 @@ udpPort.on("message", function (oscMsg) {
   if (oscMsg.address === "/gyrosc/fl4me/quat") {
     let [w, x, y, z] = oscMsg.args;
     let mappedW = clamp(Math.floor(w * 127), 10, 110);
-    let mappedX = clamp(Math.floor(x * 127), 10, 110);
+    // let mappedX = clamp(Math.floor(x * 127), 10, 110);
     let mappedY = clamp(Math.floor(y * 127), 10, 110);
     // let mappedZ = clamp(Math.floor(z * 127), 10, 110);
     midiOut.sendMessage([176, 4, mappedW]);
-    midiOut.sendMessage([176, 5, mappedX]);
+    // midiOut.sendMessage([176, 5, mappedX]);
     midiOut.sendMessage([176, 6, mappedY]);
     // midiOut.sendMessage([176, 7, mappedZ]);
-    console.log('Quaternion:', w, x, y, z, '-> CC4:', mappedW, 'CC5:', mappedX, 'CC6:', mappedY, /*'CC7:', mappedZ*/);
+    console.log('Quaternion:', w, x, y, z, '-> CC4:', mappedW, /*'CC5:', mappedX,*/ 'CC6:', mappedY, /*'CC7:', mappedZ*/);
   }
 });
 
